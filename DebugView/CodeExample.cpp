05cb9f347b2fa4f820ddd0e0c62d830ead175646
sunmeng <meng.sun@tomosight.com>
2023-12-01 16:34:27 +0800

    添加距离测试

--- DebugView/CodeExample.cpp
+++ DebugView/CodeExample.cpp
@@ -15,6 +15,7 @@
 #include <vtkPointData.h>
 #include <vtkPolyDataMapper.h>
 #include <vtkProperty.h>
+#include <vtkProperty2D.h>
 #include <vtkCylinderSource.h>
 #include <vtkOutlineFilter.h>
 #include <vtkJPEGReader.h>
@@ -39,6 +40,7 @@
 #include <vtkAnnotatedCubeActor.h>
 #include <vtkOrientationMarkerWidget.h>
 #include <vtkCameraOrientationWidget.h>
+#include <vtkPolyDataMapper2D.h>
 #include <vtkfmt/core.h>
 #include <vtkfmt/format.h>
 #include <vtkfmt/chrono.h>
@@ -48,6 +50,78 @@
 #include <VtkViewer.h>
 
 #include "Common.h"
+
+namespace
+{
+	// 点p到直线（lineP0，lineP1）的投影坐标
+	std::pair<double, double> projectionFromPoint2Line(const double* lineP0, const double* lineP1, const double* p)
+	{
+		const auto x0 = p[0];
+		const auto y0 = p[1];
+		const auto lp0x = lineP0[0];
+		const auto lp0y = lineP0[1];
+		const auto lp1x = lineP1[0];
+		const auto lp1y = lineP1[1];
+
+		if (lp0x == lp1x)
+		{
+			return { lp0x, p[1] };
+		}
+		else
+		{
+			const auto k = (lp0y - lp1y) / (lp0x - lp1x);
+			const auto b = lp0y - k * lp0x;
+			const auto x1 = (k * (y0 - b) + x0) / (k * k + 1);
+			const auto y1 = k * x1 + b;
+			return { x1, y1 };
+		}
+	}
+
+	// 此接口在线的x0==x1且y0==y1的时候会输出nan
+	double distancePointToLine(const double origin[3], const double p1[3], const double p2[3])
+	{
+		//	  origin
+		//		/\
+		//     /  \   
+		//	 b/	   \a	
+		//   /		\		  
+		// p1--------p2
+		//      c
+		//通过向量求模长
+		//1、根据三点构建三角形，利用余弦定理求出origin与p1构成的线 和 p1,p2构成的线的角度
+		//2、利用正弦定理，向量（origin-p1）的模乘以sinA
+		//			b2+c2-a2
+		//cos(p1) =----------
+		//			   2bc
+		double b = std::sqrt(std::pow(origin[0] - p1[0], 2) + std::pow(origin[1] - p1[1], 2) + std::pow(origin[2] - p1[2], 2));
+		double c = std::sqrt(std::pow(p2[0] - p1[0], 2) + std::pow(p2[1] - p1[1], 2) + std::pow(p2[2] - p1[2], 2));
+		double a = std::sqrt(std::pow(origin[0] - p2[0], 2) + std::pow(origin[1] - p2[1], 2) + std::pow(origin[2] - p2[2], 2));
+		double value = (b * b + c * c - a * a) / (2 * b * c);
+		double A = std::acos(value);
+		double distance = std::abs(b * std::sin(A));
+		return distance;
+	}
+
+	double distancePointToLine2(const double origin[2], const double p1[2], const double p2[2])
+	{
+		const auto lp0x = p1[0];
+		const auto lp0y = p1[1];
+		const auto lp1x = p2[0];
+		const auto lp1y = p2[1];
+		const auto x0 = origin[0];
+		const auto y0 = origin[1];
+		if (lp0x == lp1x)
+		{
+			return std::abs(lp0x-x0);
+		}
+		else
+		{
+			const auto k = (lp0y - lp1y) / (lp0x - lp1x);
+			const auto b = lp0y - k * lp0x;
+			return std::abs(k*x0 - y0 + b) / std::sqrt(k * k + 1);
+		}
+	}
+}
 
 struct ExampleAppLog
 {
@@ -436,6 +510,104 @@ void leftRightScreen()
 	vtkViewer.render();
 }
 
+void distanceTest()
+{
+	static VtkViewer myView;
+	static bool init = false;
+	static auto lineSource = vtkSmartPointer<vtkLineSource>::New();
+	static vtkNew<vtkPolyData> pointPolyData;
+	static vtkIdType pid[1];
+
+	auto updateProjectPoint = []
+		{
+			const auto pPoint = pointPolyData->GetPoint(pid[0]);
+			const auto lp0 = lineSource->GetPoint1();
+			const auto lp1 = lineSource->GetPoint2();
+			const auto [x, y] = ::projectionFromPoint2Line(lp0, lp1, pPoint);
+
+			static vtkSmartPointer<vtkLineSource> sphereSource;
+			static auto text = vtkSmartPointer<vtkTextActor>::New();
+			if (!sphereSource.GetPointer())
+			{
+				sphereSource = vtkSmartPointer<vtkLineSource>::New();
+				sphereSource->Update();
+				auto mapper = vtkSmartPointer<vtkPolyDataMapper2D>::New();
+				mapper->SetInputConnection(sphereSource->GetOutputPort());
+				auto actor = vtkSmartPointer<vtkActor2D>::New();
+				actor->SetMapper(mapper);
+				actor->GetProperty()->SetColor(1, 1, 0);
+				myView.addActor(actor);
+				text->GetTextProperty()->SetColor(0, 1, 0);
+				text->GetTextProperty()->SetOpacity(0.5);
+				text->GetTextProperty()->SetBackgroundColor(1, 0, 0);
+				text->GetTextProperty()->SetBackgroundOpacity(0.5);
+				text->GetTextProperty()->SetFontSize(18);
+				text->GetTextProperty()->SetJustification(VTK_TEXT_RIGHT);
+				//text->GetPositionCoordinate()->SetCoordinateSystemToWorld();
+				myView.addActor(text);
+			}
+			sphereSource->SetPoint1(pPoint);
+			sphereSource->SetPoint2(x, y, 0);
+			text->SetPosition((pPoint[0] + x) / 2, (pPoint[1] + y) / 2);
+			text->SetInput(fmt::format("{:.2f}",distancePointToLine2(pPoint, lp0, lp1)).c_str());
+		};
+
+	if (!init)
+	{
+		init = true;
+
+		{
+			lineSource->SetPoint1(500.0, 500.0, 0.0);  // 起点坐标 
+			lineSource->SetPoint2(900.0, 500.0, 0.0);  // 终点坐标
+			lineSource->Update();
+			auto mapper = vtkSmartPointer<vtkPolyDataMapper2D>::New();
+			mapper->SetInputConnection(lineSource->GetOutputPort());
+			auto lineActor = vtkSmartPointer<vtkActor2D>::New();
+			lineActor->SetMapper(mapper);
+			myView.addActor(lineActor);
+		}
+
+		{
+			vtkNew<vtkPoints> points;
+			const float p[3] = { 800.0, 600.0, 0.0 };
+			vtkNew<vtkCellArray> vertices;
+			pid[0] = points->InsertNextPoint(p);
+			vertices->InsertNextCell(1, pid);
+			pointPolyData->SetPoints(points);
+			pointPolyData->SetVerts(vertices);
+			vtkNew<vtkPolyDataMapper2D> mapper;
+			mapper->SetInputData(pointPolyData);
+			vtkNew<vtkActor2D> actor;
+			actor->SetMapper(mapper);
+			actor->GetProperty()->SetColor(1,0,0);
+			actor->GetProperty()->SetPointSize(20);
+			myView.addActor(actor);
+		}
+
+		updateProjectPoint();
+		myView.getRenderer()->SetBackground(0, 0, 0);
+	}
+
+	if (ImGui::DragScalarN("linePoint1", ImGuiDataType_Double, lineSource->GetPoint1(), 2, 0.5f))
+	{
+		lineSource->Modified();
+		updateProjectPoint();
+	}
+	if (ImGui::DragScalarN("linePoint2", ImGuiDataType_Double, lineSource->GetPoint2(), 2, 0.5f))
+	{
+		lineSource->Modified();
+		updateProjectPoint();
+	}
+	if (const auto pPoint = pointPolyData->GetPoint(pid[0]); ImGui::DragScalarN("pPoint", ImGuiDataType_Double, pPoint, 2, 0.5f))
+	{
+		auto newPoints = vtkSmartPointer<vtkPoints>::New();
+		newPoints->InsertNextPoint(pPoint);
+		pointPolyData->SetPoints(newPoints);
+		updateProjectPoint();
+	}
+	myView.render();
+}
+
 void coordinateTest()
 {
 	static VtkViewer myView;
@@ -444,6 +616,7 @@ void coordinateTest()
 	static int eventPosition[2]{};
 	static double worldPosition[3]{};
 	static double worldPosition_[3]{};
+	static double worldPosition__[3]{};
 
 	if (!init)
 	{
@@ -476,6 +649,8 @@ void coordinateTest()
 					renderer->SetDisplayPoint(eventPosition[0], eventPosition[1], 0);
 					renderer->DisplayToWorld();
 					renderer->GetWorldPoint(worldPosition_);
+					// 3
+					vtkInteractorObserver::ComputeDisplayToWorld(renderer, eventPosition[0], eventPosition[1], 0, worldPosition__);
 
 					std::cout << "Picking pixel: " << interactor->GetEventPosition()[0]
 						<< " " << interactor->GetEventPosition()[1] << std::endl;
@@ -497,6 +672,7 @@ void coordinateTest()
 	ImGui::InputScalarN("EventPosition", ImGuiDataType_S32, eventPosition, static_cast<int>(std::size(eventPosition)), nullptr, nullptr, nullptr, ImGuiInputTextFlags_ReadOnly);
 	ImGui::InputScalarN("WorldPosition", ImGuiDataType_Double, worldPosition, static_cast<int>(std::size(worldPosition)), nullptr, nullptr, nullptr, ImGuiInputTextFlags_ReadOnly);
 	ImGui::InputScalarN("WorldPosition_", ImGuiDataType_Double, worldPosition_, static_cast<int>(std::size(worldPosition_)), nullptr, nullptr, nullptr, ImGuiInputTextFlags_ReadOnly);
+	ImGui::InputScalarN("WorldPosition__", ImGuiDataType_Double, worldPosition__, static_cast<int>(std::size(worldPosition__)), nullptr, nullptr, nullptr, ImGuiInputTextFlags_ReadOnly);
 
 	myView.render();
 }
@@ -1095,6 +1271,16 @@ void SubjectObserverTest()
 			log.AddLog(fmt::format("{} Interactor Style\n", std::chrono::system_clock::now()).c_str());
 			vtkInteractorStyleTrackballCamera::OnLeftButtonDown();
 		}
+
+		void OnChar() override
+		{
+			log.Add(fmt::format("{} {}", __func__, this->Interactor->GetKeyCode()));
+		}
+
+		void OnKeyPress() override
+		{
+			log.Add(fmt::format("{} {}", __func__, this->GetInteractor()->GetKeySym()));
+		}
 	};
 
 	class MyCallback : public vtkCommand
@@ -1323,6 +1509,11 @@ void renderExample()
 		ImGuiTabBarFlags tab_bar_flags = ImGuiTabBarFlags_None;
 		if (ImGui::BeginTabBar("MyTabBar", tab_bar_flags))
 		{
+			if (ImGui::BeginTabItem("Distance"))
+			{
+				distanceTest();
+				ImGui::EndTabItem();
+			}
 			if (ImGui::BeginTabItem("Anchor"))
 			{
 				anchorTest();
